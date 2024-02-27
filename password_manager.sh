4cf70346f2779b43776612a9892c8a07ef220710
sousuke333 <ergo.console.log02@gmail.com>
2024-02-27 16:44:33 +0900

    Functionalize the password registration process

--- password_manager.sh
+++ password_manager.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
 echo "パスワードマネージャーへようこそ！"
-# 要件整理
-# welcomeメッセージの追加、選択肢を出す
-# 入力が"Add Password,Get Password,Exit"以外なら再度入力を求める
-# Add Passwordが入力されたらデータ登録の処理
-# Get Passwordが入力されたら
-# サービス名の入力を求める
-# data.txtから検索してヒットしたら表示
-# なければ"そのサービスは登録されていません。"とし最初の選択肢に
-# Exitが入力されたら"Thank you!"と表示しプログラム終了
 
+register_password () {
 echo -n "サービス名を入力してください："
-
 read service_name
 while [ -z "$service_name" ]
 do
@@ -20,9 +11,7 @@ do
   read service_name
 done
 
-
 echo -n "ユーザー名を入力してください："
-
 read user_name
 while [ -z "$user_name" ]
 do
@@ -30,9 +19,7 @@ do
   read user_name
 done
 
-
 echo -n "パスワードを入力してください："
-
 read password
 while [ -z "$password" ]
 do
@@ -40,7 +27,19 @@ do
   read password
 done
 
-
 echo "$service_name:$user_name:$password" >> ./data.txt
 echo "Thank you!"
+}
+
+
+# 要件整理
+# welcomeメッセージの追加、選択肢を出す
+# 入力が"Add Password,Get Password,Exit"以外なら再度入力を求める
+# Add Passwordが入力されたらデータ登録の処理
+register_password
+# Get Passwordが入力されたら
+# サービス名の入力を求める
+# data.txtから検索してヒットしたら表示
+# なければ"そのサービスは登録されていません。"とし最初の選択肢に
+# Exitが入力されたら"Thank you!"と表示しプログラム終了
 
